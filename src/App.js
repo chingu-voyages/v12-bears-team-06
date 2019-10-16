6ecec67cfeb2d526f3ebcc3c23617786f53e488d
Josy Hartig <josy_hartig@yahoo.de>
2019-10-16 22:51:22 +0800

    add routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
-
-import Layout from './containers/Layout';
+import React, {Suspense} from 'react';
+import {Route, Switch, Redirect} from 'react-router-dom';
+import Home from './containers/Home';
+import Dashboard from './containers/Dashboard';
+import Loading from './components/Loading/Loading';
 import './style.scss';
 
 function App() {
+
   return (
     <div className="app">
-      <Layout />
+      <Suspense fallback={<Loading/>}>
+        <Switch>
+          <Route path="/board" component={Dashboard} />
+          <Route path="/" exact component={Home} />
+          <Redirect to="/" />
+        </Switch>
+      </Suspense>
     </div>
   );
 }
